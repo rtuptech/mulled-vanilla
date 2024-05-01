b88cf28b188527ccae22d099b7c770b04eb00303
rtuptech <71769011+rtuptech@users.noreply.github.com>
2024-05-01 06:46:22 +0300

    add canvas animation

--- animation.js
+++ animation.js
@@ -1,72 +1,30 @@
-// JavaScript
-          var lastScrollPosition = 0;
-          window.addEventListener('scroll', function() {
-            var scrollPosition = window.scrollY;
-            var imgProduct1 = document.querySelector('.img-product-1');
-            var bagOpen = document.querySelector('.bag-open');
-            var bgAnimate1 = document.querySelector('.bg-animate-1');
-            if (scrollPosition < lastScrollPosition) {
-              // Page is scrolling up
-              imgProduct1.style.transform = `translateY(${scrollPosition}px) rotate(0deg) translateX(0)`;
-              imgProduct1.style.left = '';
-            } else {
-              // Page is scrolling down
-              imgProduct1.style.transform = `translateY(${scrollPosition}px) rotate(-8deg) translateX(-20vw)`;
-              imgProduct1.style.left = '50%';
-            }
-            if (scrollPosition > lastScrollPosition) {
-              // Page is scrolling down
-              bgAnimate1.style.transform = 'scale(1)';
-            } else {
-              // Page is scrolling up
-              bgAnimate1.style.transform = '';
-            }
-            if (scrollPosition > lastScrollPosition) {
-              // Page is scrolling down
-              document.querySelector('img.bg-animate-2').style.cssText = 'left: -35rem; bottom: -35rem; transform: scale(1) ';
-              document.querySelector('img.bg-animate-3').style.cssText = 'left: -55rem; transform: scale(1)';
-              document.querySelector('img.bg-animate-4').style.cssText = 'left: -40rem; bottom: 38rem; transform: scale(1)';
-              document.querySelector('img.bg-animate-5').style.cssText = 'left: -35rem; bottom: 55rem; transform: scale(1)';
-              document.querySelector('img.bg-animate-6').style.cssText = 'right: -35rem; bottom: 30rem; transform: scale(1)';
-              document.querySelector('img.bg-animate-7').style.cssText = 'right: -27rem; bottom: 45rem; transform: scale(1)';
-              document.querySelector('img.bg-animate-8').style.cssText = 'right: -35rem; bottom: -10rem; transform: scale(1)';
-              document.querySelector('img.bg-animate-9').style.cssText = 'right: -25rem; bottom: -35rem; transform: scale(1)';
-            } else {
-              document.querySelector('img.bg-animate-2').style.cssText = '';
-              document.querySelector('img.bg-animate-3').style.cssText = '';
-              document.querySelector('img.bg-animate-4').style.cssText = '';
-              document.querySelector('img.bg-animate-5').style.cssText = '';
-              document.querySelector('img.bg-animate-6').style.cssText = '';
-              document.querySelector('img.bg-animate-7').style.cssText = '';
-              document.querySelector('img.bg-animate-8').style.cssText = '';
-              document.querySelector('img.bg-animate-9').style.cssText = ')';
-            }
-            // Update the last scroll position
-            lastScrollPosition = scrollPosition;
-          });
-          document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-              e.preventDefault();
-              var target = document.querySelector(this.getAttribute('href'));
-              var targetPosition = target.getBoundingClientRect().top;
-              var startPosition = window.pageYOffset;
-              var distance = targetPosition;
-              var duration = 1500; // Increase this value to slow down the scroll speed
-              var start = null;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+canvas.width = 1778;
+canvas.height = 1080;
+const frameCount = 44;
+const currentFrame = index => (`https://cdn.shopify.com/s/files/1/0659/6080/4509/files/${(index + 1).toString().padStart(4, '0')}.png`);
+const images = []
+const airpods = {
+  frame: 0
+};
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  images.push(img);
+}
+gsap.to(airpods, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: "none",
+  scrollTrigger: {
+		scrub: 0.5
+  },
+  onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
+});
+images[0].onload = render;
 
-              function step(timestamp) {
-                if (!start) start = timestamp;
-                var progress = timestamp - start;
-                window.scrollTo(0, easeInOutQuad(progress, startPosition, distance, duration));
-                if (progress < duration) window.requestAnimationFrame(step);
-              }
-
-              function easeInOutQuad(t, b, c, d) {
-                t /= d / 2;
-                if (t < 1) return c / 2 * t * t + b;
-                t--;
-                return -c / 2 * (t * (t - 2) - 1) + b;
-              }
-              window.requestAnimationFrame(step);
-            });
-          });
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[airpods.frame], 0, 0);
+}
